e43fd2a81a753bdbf9874a22332a50dae5bf2655
jamdev <jamissonmachado@gmail.com>
2026-07-21 22:07:04 -0300

    layout

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -3,6 +3,12 @@ import React from "react";
 
 export default function AuthLayout() {
   return (
-    <Stack screenOptions={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="home" />
+      <Stack.Screen name="planilha" />
+      <Stack.Screen name="mural" />
+      <Stack.Screen name="perfilpro" />
+      <Stack.Screen name="alunos" />
+    </Stack>
   );
 }
